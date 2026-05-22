f124736943a894833b43b0f3909217d11a5f4b4f
nichoth <nichoth@nichoth.com>
2026-05-22 10:45:57 -0700

    add .error method

--- example/index.ts
+++ example/index.ts
@@ -7,16 +7,18 @@ console.log('is dev???', import.meta.env.DEV)
 
 const debug = Debug('fooo')
 
-localStorage.setItem('DEBUG', 'abc:*')
+localStorage.setItem('DEBUG', 'fooo')
 
 let dynamicDebugger:Debugger
 
 if (import.meta.env.DEV) {
     const { createDebug } = (await import('../src/browser/index.js'))
-    dynamicDebugger = createDebug('abc:123')
+    dynamicDebugger = createDebug('fooo')
 } else {
     dynamicDebugger = noop
 }
+
+debug.error('testing errors with staticly imported "fooo" debugger')
 
 dynamicDebugger('hello dynamic imports')
 
@@ -25,8 +27,18 @@ debug('debug works')
 const debug3 = Debug('barrr')
 debug3('barrrrr')
 
+// should not see this
+// if we set in localStorage DEBUG = 'barrr', then it should log
+debug3.error('testing errors with staticly imported "barrr" debugger')
+
 const debug4 = Debug('bazzz')
 debug4('bazzzzzzz')  // should not log
+
+// @ts-expect-error dev
+window.debug = {
+    fooo: debug,
+    barrr: debug3
+}
 
 // testing the * variable
 const debug7 = Debug('quxxx')
@@ -38,7 +50,7 @@ debug8('hello eight')
 
 // testing boolean true - should always log regardless of localStorage
 const debugForced = Debug(true)
-debugForced('This should always log because we passed boolean true!')
+debugForced('This should always log because we passed boolean true.')
 
 // testing boolean false - should return noop
 const debugNoop = Debug(false)
